dc9638e0bce4095b782edfbd467040e486a4ab81
Juan2k-1 <juan2k-1@MGCSS>
2024-05-27 20:43:52 +0200

    Añadido saludo inicial en el welcome según usuario logeado

--- Acme-Dancer/src/main/java/controllers/WelcomeController.java
+++ Acme-Dancer/src/main/java/controllers/WelcomeController.java
@@ -1,8 +1,8 @@
 /*
  * WelcomeController.java
- * 
+ *
  * Copyright (C) 2018 Universidad de Sevilla
- * 
+ *
  * The use of this project is hereby constrained to the conditions of the
  * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
@@ -13,31 +13,40 @@ package controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import security.LoginService;
+import security.UserAccount;
 
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
 
 	// Constructors -----------------------------------------------------------
+	@Autowired
+	LoginService loginService;
+
 
 	public WelcomeController() {
 		super();
 	}
 
-	// Index ------------------------------------------------------------------		
+	// Index ------------------------------------------------------------------
 
 	@RequestMapping(value = "/index")
-	public ModelAndView index(@RequestParam(required = false, defaultValue = "John Doe") final String name) {
+	public ModelAndView index(@RequestParam(required = false, defaultValue = "John Doe") String name) {
 		ModelAndView result;
 		SimpleDateFormat formatter;
 		String moment;
 
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
+		final UserAccount userAccount = LoginService.getPrincipal();
+		name = userAccount.getUsername();
 
 		result = new ModelAndView("welcome/index");
 		result.addObject("name", name);
